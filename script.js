80f8e7cc9d990be79f1375a1adaa85b45421a976
VanessaTrajano <vanessapalmeirakelmertrajano@gmail.com>
2022-01-18 10:54:41 -0300

    Subindo a dinâmica da aula 3 de lógica

---
+++ script.js
@@ -0,0 +1,37 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+
+console.log("Atividade 1")
+
+for(let i = 1; i <= 10; i++){
+    console.log(i)
+}
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+
+console.log("Atividade 2")
+
+let contador = 10
+
+while(contador >= 1){
+    console.log(contador)
+    contador--
+}
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+console.log("Atividade 3")
+
+let cont = 1
+
+while (cont <= 100){
+    console.log(cont)
+    cont +=2
+}
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+
+console.log("Atividade 4")
+
+for(i = 0; i <= 100; i += 2){
+    console.log(i)
+}
